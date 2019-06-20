9ad2c5656ccdd0b92a0c3ae0901a59eeaf5b63fa
sky06walker <c.weihon@gmail.com>
2019-06-20 11:16:24 +0800

    Commit for reset password

--- Housekeeper/Housekeeper/Controllers/UsersMasterController.cs
+++ Housekeeper/Housekeeper/Controllers/UsersMasterController.cs
@@ -136,32 +136,39 @@ namespace Housekeeper.Controllers
                 var v = dc.UsersMasters.Where(a => a.UserName == login.Username).FirstOrDefault();
                 if (v != null)
                 {
-                    if (string.Compare(Crypto.Hash(login.Password),v.Password) == 0)
+                    if (v.isEmailVerified == false)
                     {
-                        //Process to save login
-                        int timeout = login.SaveLogin ? 525600 : 1; //525600 min = 1 year
-                        var ticket = new FormsAuthenticationTicket(login.Username, login.SaveLogin, timeout);
-                        string encrypted = FormsAuthentication.Encrypt(ticket);
-                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted)
-                        {
-                            Expires = DateTime.Now.AddMinutes(timeout),
-                            HttpOnly = true
-                        };
-                        Response.Cookies.Add(cookie);
-
-                        if (Url.IsLocalUrl(ReturnUrl))
-                        {
-                            return Redirect(ReturnUrl);
-                        }
-                        else
-                        {
-                            return RedirectToAction("Index", "Home");
-                        }
+                        message = "Please activate your account to continue login.";
                     }
                     else
                     {
-                        message = "Invalid password!";
-                        ModelState.AddModelError("InvalidPassword", message);
+                        if (string.Compare(Crypto.Hash(login.Password), v.Password) == 0)
+                        {
+                            //Process to save login
+                            int timeout = login.SaveLogin ? 525600 : 1; //525600 min = 1 year
+                            var ticket = new FormsAuthenticationTicket(login.Username, login.SaveLogin, timeout);
+                            string encrypted = FormsAuthentication.Encrypt(ticket);
+                            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted)
+                            {
+                                Expires = DateTime.Now.AddMinutes(timeout),
+                                HttpOnly = true
+                            };
+                            Response.Cookies.Add(cookie);
+
+                            if (Url.IsLocalUrl(ReturnUrl))
+                            {
+                                return Redirect(ReturnUrl);
+                            }
+                            else
+                            {
+                                return RedirectToAction("Index", "Home");
+                            }
+                        }
+                        else
+                        {
+                            message = "Invalid password!";
+                            ModelState.AddModelError("InvalidPassword", message);
+                        }
                     }
                 }
                 else
@@ -244,5 +251,115 @@ namespace Housekeeper.Controllers
         }
         #endregion
 
+        #region//Forgot password
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(string Email)
+        {
+            string message = "";
+            bool Status = false;
+
+            //Verify email address
+            using (HousekeeperDataEntities dc=new HousekeeperDataEntities())
+            {
+                var v = dc.UsersMasters.Where(a => a.Email == Email).FirstOrDefault();
+                if (v != null)
+                {
+                    SendForgotPwdEmail(v.UserId, Email, v.UserName);
+                    message = "Reset password email has been sent to your registered email account.";
+                    Status = true;
+                }
+                else
+                {
+                    message = "Email not found.";
+                }
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+
+        #region// Public procedure Email forgot password
+        [NonAction]
+        public void SendForgotPwdEmail(int userId, string emailAddress, string userName)
+        {
+            var verifyURL = "/UsersMaster/ResetPassword/" + userId;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyURL);
+
+            var fromEmail = new MailAddress("c.weihon@gmail.com", "Housekeeper");
+            var toEmail = new MailAddress(emailAddress, userName);
+            var fromEmailPwd = "810821n811204";
+            string subject = "Housekeeper Reset Password";
+            string body = "<br/><br/>Dear Housekeeper," +
+                "<br/>Please click below link for reset your login password." +
+                "<br/><br/><a href='" + link + "'>Reset Password</a>";
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPwd)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+        #endregion
+
+        #region//Reset password
+        [HttpGet]
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ResetPassword(string id, UsersMaster user)
+        {
+            bool Status = false;
+            string message = "";
+            int userID = Convert.ToInt32(id);
+            
+            if (id != null)
+            {
+                using (HousekeeperDataEntities dc = new HousekeeperDataEntities())
+                {
+                    dc.Configuration.ValidateOnSaveEnabled = false;
+                    var v = dc.UsersMasters.Where(a => a.UserId == userID).FirstOrDefault();
+                    {
+                        if (v != null)
+                        {
+                            v.Password = Crypto.Hash(user.Password);
+                            dc.SaveChanges();
+                            message = "Password changed!";
+                            Status = true;
+                        }
+                        else
+                        {
+                            message = "Invalid request!";
+                        }
+                    }
+                }
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
     }
 }
